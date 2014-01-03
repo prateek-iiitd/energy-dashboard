3055754d682cd12466ff46c90f41c64a968c9d8d
Prateek <prateek11077@iiitd.ac.in>
2014-01-04 01:29:15 +0530

    Meter parameters updated. Their units added. OnHours and PowerIntr are long values which are parsed and evaluated differently. Timeseries for these created with data_type as 'long'.

--- Deployment/modbus_usb.py
+++ Deployment/modbus_usb.py
@@ -1,14 +1,18 @@
 import os
 from os.path import join
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
 from smap.driver import SmapDriver, util
 import struct
 from smap import core, actuate
 from serial import SerialException
 
+
 class RateActuator(actuate.NStateActuator):
     """Actuator to set the Polling Rate for a meter.
     """
+
     def setup(self, opts):
         actuate.NStateActuator.setup(self, opts)
         self.meter = opts.get('meter')
@@ -46,8 +50,9 @@ class ModbusUSBDriver(SmapDriver):
 
         self.meters = []
         for x in xrange(0, self.meter_count):
-            self.meters += [ Meter(self.METERS[x], self.RATES[x], self.FLOORS[x], self.TYPES[x], self.MODELS[x], self.FLATNUMS[x],
-                      self.BLOCKS[x], self.WINGS[x], self.LOADTYPES[x], self.SUBLOADTYPES[x], self.SUPPLYTYPES[x]) ]
+            self.meters += [
+                Meter(self.METERS[x], self.RATES[x], self.FLOORS[x], self.TYPES[x], self.MODELS[x], self.FLATNUMS[x],
+                      self.BLOCKS[x], self.WINGS[x], self.LOADTYPES[x], self.SUBLOADTYPES[x], self.SUPPLYTYPES[x])]
 
         del (self.RATES)
         del (self.METERS)
@@ -92,14 +97,18 @@ class ModbusUSBDriver(SmapDriver):
         for x in self.queue[self.current]:
             try:
                 t = util.now()
-                self.res = self.client.read_holding_registers(3900, 62, unit=x.Id)
+                self.res = self.client.read_holding_registers(3900, 100, unit=x.Id)
 
                 if (self.res == None):
                     continue
 
                 for y in xrange(0, len(self.parameters[x.Model])):
                     read_reg = self.reading_registers[x.Model][y]
-                    value = convert((self.res.registers[read_reg + 1] << 16) + self.res.registers[read_reg])
+                    if (read_reg==92 or read_reg==98):
+                        decoder = BinaryPayloadDecoder.fromRegisters(self.res.registers[read_reg:read_reg+2], endian=Endian.Big)
+                        value = decoder.decode_32bit_uint()
+                    else:
+                        value = convert((self.res.registers[read_reg + 1] << 16) + self.res.registers[read_reg])
                     self.add('/Meter' + str(x.Id) + '/' + self.parameters[x.Model][y], t, value)
 
             except SerialException:
@@ -111,17 +120,16 @@ class ModbusUSBDriver(SmapDriver):
             popped = self.queue[self.current].pop(0)
             next_index = (self.current + popped.Rate) % self.SLOWEST_POSSIBLE_RATE
 
-            while (len(self.queue[next_index])==2):
-                next_index+=1
+            while (len(self.queue[next_index]) == 2):
+                next_index += 1
                 if (next_index == self.SLOWEST_POSSIBLE_RATE):
-                    next_index=0
+                    next_index = 0
 
             self.queue[next_index].append(popped)
 
-
         self.current = (self.current + 1) % self.SLOWEST_POSSIBLE_RATE
 
-    def read_configurations(self,opts):
+    def read_configurations(self, opts):
     ##Get information from config file about meters to be polled and the rates at which they are to polled.
         ##Get meter specific Metadata that is supplied by the config file.
 
@@ -154,7 +162,7 @@ class ModbusUSBDriver(SmapDriver):
             self.SUBLOADTYPES = [str(x) for x in opts.get('SUBLOADTYPES')]
             self.SUPPLYTYPES = [str(x) for x in opts.get('SUPPLYTYPES')]
 
-                ##Connection Parameters.
+            ##Connection Parameters.
         self.BAUD_RATE = int(opts.get('BAUD_RATE', 9600))
         self.STOP_BITS = int(opts.get('STOP_BITS', 1))
         self.BYTE_SIZE = int(opts.get('BYTE_SIZE', 8))
@@ -167,28 +175,35 @@ class ModbusUSBDriver(SmapDriver):
 
 
     def configure_parameters(self):
-        self.reading_registers = {'EM6400': [2, 6, 10, 14, 18, 22, 32, 36, 46, 50, 60],
-                                  'EM6433': [2, 18, 32, 46, 60],
-                                  'EM6436': [2, 18, 22, 32, 36, 46, 50, 60]}
+        self.reading_registers = {'EM6400': [2, 6, 12, 14, 24, 26, 58, 60, 92, 98],
+                                  'EM6433': [2, 12, 18, 32, 46, 60, 92, 98],
+                                  'EM6436': [2, 6, 12, 18, 26, 32, 46, 60, 92, 98]}
 
         self.parameters = {
-        'EM6400': ['Power', 'PowerFactor', 'Voltage', 'Frequency', 'PowerPhase1', 'PowerFactorPhase1', 'PowerPhase2',
-                   'PowerFactorPhase2', 'PowerPhase3', 'PowerFactorPhase3', 'Energy'],
-        'EM6433': ['Power', 'PowerPhase1', 'PowerPhase2', 'PowerPhase3', 'Energy'],
-        'EM6436': ['Power', 'PowerPhase1', 'PowerFactorPhase1', 'PowerPhase2', 'PowerFactorPhase2', 'PowerPhase3',
-                   'PowerFactorPhase3', 'Energy']}
+            'EM6400': ['Power', 'PowerFactor', 'Current', 'Frequency', 'VoltagePhase1Phase2', 'VoltagePhase1', 'ApparentEnergy',
+                       'Energy', 'OnHours', 'PowerIntr'],
+            'EM6433': ['Power', 'Current', 'PowerPhase1', 'PowerPhase2', 'PowerPhase3', 'Energy', 'OnHours',
+                       'PowerIntr'],
+            'EM6436': ['Power', 'PowerFactor', 'Current', 'PowerPhase1', 'VoltagePhase1', 'PowerPhase2', 'PowerPhase3',
+                       'Energy', 'OnHours', 'PowerIntr']}
 
         self.units = {'Power': 'Watts',
-                  'PowerFactor': '',
-                  'Voltage': 'Volts',
-                  'Frequency': 'Hertz',
-                  'Energy': 'Watt-Hours',
-                  'PowerPhase1': 'Watts',
-                  'PowerPhase2': 'Watts',
-                  'PowerPhase3': 'Watts',
-                  'PowerFactorPhase1': '',
-                  'PowerFactorPhase2': '',
-                  'PowerFactorPhase3': ''}
+                      'PowerFactor': '',
+                      'Voltage': 'Volts',
+                      'VoltagePhase1Phase2': 'Volts',
+                      'VoltagePhase1': 'Volts',
+                      'Frequency': 'Hertz',
+                      'Energy': 'Watt-Hours',
+                      'ApparentEnergy': 'Volt-Ampere-Hours',
+                      'Current': 'Amperes',
+                      'PowerPhase1': 'Watts',
+                      'PowerPhase2': 'Watts',
+                      'PowerPhase3': 'Watts',
+                      'PowerFactorPhase1': '',
+                      'PowerFactorPhase2': '',
+                      'PowerFactorPhase3': '',
+                      'OnHours': '',
+                      'PowerIntr': ''}
 
     def configure_meter_streams_metadata(self):
     ##Adding collections to represent meters.
@@ -211,9 +226,9 @@ class ModbusUSBDriver(SmapDriver):
                     'MeterID': x.Id,
                     'Block': x.Block,
                     'Wing': x.Wing,
-                    'LoadType' : x.LoadType,
-                    'SubLoadType' : x.SubLoadType,
-                    'SupplyType' : x.SupplyType
+                    'LoadType': x.LoadType,
+                    'SubLoadType': x.SubLoadType,
+                    'SupplyType': x.SupplyType
                 }
             }
             ## Add actuator to meter.
@@ -221,8 +236,12 @@ class ModbusUSBDriver(SmapDriver):
                               timezone='Asia/Kolkata', setup={'states': [str(i) for i in range(1, 31)], 'meter': x})
 
             for y in self.parameters[x.Model]:
-                ts = self.add_timeseries('/Meter' + str(x.Id) + '/' + y, self.units[y], data_type='double',
-                                         timezone='Asia/Kolkata')
+                if y == 'PowerIntr' or y == 'OnHours':
+                    ts = self.add_timeseries('/Meter' + str(x.Id) + '/' + y, self.units[y], data_type='long',
+                                             timezone='Asia/Kolkata')
+                else:
+                    ts = self.add_timeseries('/Meter' + str(x.Id) + '/' + y, self.units[y], data_type='double',
+                                             timezone='Asia/Kolkata')
                 ts['Metadata'] = {'Extra': {'PhysicalParameter': y}}
 
 
