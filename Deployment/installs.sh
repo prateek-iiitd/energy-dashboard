8edb6ed58c8775e5cb4ead5d224135847aae9de9
Prateek <prateek11077@iiitd.ac.in>
2014-04-05 19:30:59 +0530

    Added dependency for pyopenssl.

--- Deployment/installs.sh
+++ Deployment/installs.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
 apt-get update
+apt-get install git -y
 apt-get install python-pip -y
 apt-get install python-dev -y
 apt-get install python-numpy -y
 apt-get install python-scipy -y
-pip install smap
+sudo apt-get install libffi-dev
 pip install pyOpenSSL
 apt-get install libcurl4-gnutls-dev librtmp-dev -y
 pip install pycURL
 pip install pymodbus
+sudo pip install zope.interface twisted python-dateutil avro configobj
+git clone https://bitbucket.org/zenatix/smap
+cd smap/python
+sudo setup.py install
